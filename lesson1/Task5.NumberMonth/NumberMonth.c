8050d13945735b47144f4ce5f23dbb8cf58f4af1
Andrey Kas <kasimovskiy@mail.ru>
2020-12-22 21:19:04 +0300

    update task 5 and 6

--- lesson1/Task5.NumberMonth/NumberMonth.c
+++ lesson1/Task5.NumberMonth/NumberMonth.c
@@ -6,4 +6,21 @@ int main()
 
 	printf("Enter month from 1 to 12: ");
 	scanf_s("%d", &month);
+
+	if (month >= 3 && month <= 5)
+	{
+		printf("Season: spring");
+	}
+	else if (month >= 6 && month <= 8)
+	{
+		printf("Season: summer");
+	}
+	else if (month >= 9 && month <= 11)
+	{
+		printf("Season: autumn");
+	}
+	else
+	{
+		printf("Season: winter");
+	}
 }
--- lesson1/Task6.Declination/Declination.c
+++ lesson1/Task6.Declination/Declination.c
@@ -5,4 +5,24 @@ int main()
 	int age;
 	printf("Eneter age (1 - 150): ");
 	scanf_s("%d", &age);
+
+	if (age % 10 == 1 && age % 100 != 11)
+	{
+		printf("%d год\n", age);
+	}
+	else if(age % 10 >= 2 && age % 10 <= 4)
+	{
+		if (age % 100 != 12 && age % 100 != 13 && age % 100 != 14)
+		{
+			printf("%d года\n", age);
+		}
+		else
+		{
+			printf("%d лет\n", age);
+		}
+	}
+	else
+	{
+		printf("%d лет\n", age);
+	}
 }
